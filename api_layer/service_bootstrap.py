de1057ca1fd0d0d3a0d99f70c8f45f94275b9f80
MrPratsJi <pratyushg2001@gmail.com>
2025-12-18 16:52:08 +0530

    Remove stale embedding import from app bootstrap

--- api_layer/service_bootstrap.py
+++ api_layer/service_bootstrap.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI
 from api_layer.health_probe import router as health_router
 from api_layer.recommend_router import router as recommend_router
-from semantic_index.vector_forge import load_index
-from semantic_index.embedding_gateway import get_model
 
 def create_app():
     app = FastAPI(title="SHL Assessment Recommender")
-
-    # preload heavy resources
-    load_index()
-    get_model()
-
     app.include_router(health_router)
     app.include_router(recommend_router)
     return app
